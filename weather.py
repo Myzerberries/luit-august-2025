eb51e2603ec38275d5d89d8d2b6090b0abd2dc65
Myzerberries <52772439+Myzerberries@users.noreply.github.com>
2025-08-19 21:05:44 -0500

    Modified formatting to conform to pep8 using 'black'

--- weather.py
+++ weather.py
@@ -2,17 +2,20 @@ import requests
 
 city = "London"
 
-url = 'http://api.weatherapi.com/v1/current.json?key=1f77cc8db2774ef5a3713432251408&q=' + city + '&aqi=no'
+url = (
+    "http://api.weatherapi.com/v1/current.json?key=1f77cc8db2774ef5a3713432251408&q="
+    + city
+    + "&aqi=no"
+)
 
 response = requests.get(url)
 
 weather_json = response.json()
 
-temp = weather_json.get('current').get('temp_f')
+temp = weather_json.get("current").get("temp_f")
 
-description = weather_json.get('current').get('condition').get('text')
+description = weather_json.get("current").get("condition").get("text")
 
-#description2 = weather_json(['current'])
+# description2 = weather_json(['current'])
 
-print ("Today's weather in" , city, "London is", description, "and", temp, "degrees")
-
+print("Today's weather in", city, "London is", description, "and", temp, "degrees")
